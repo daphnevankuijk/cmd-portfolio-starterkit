51a59cde41083681552f54f241a1b162e373199c
daphnevankuijk <61200861+daphnevankuijk@users.noreply.github.com>
2020-04-17 12:29:35 +0200

    laatste loodjes

---
+++ site/templates/over-mij.php
@@ -0,0 +1,26 @@
+<?php snippet('header') ?>
+
+<main>
+  <?php snippet('intro') ?>
+  <div class="row">
+    <div class="col 12 col-md-6">
+      <div class="text">
+      Ik ben afgestudeerd aan het ROC Mondriaan als Bedrijfsadministrateur. Ik ben na deze studie 
+aan het werk gegaan als medewerker personeelszaken. Hier heb ik veel geleerd op het gebied 
+van personeelszaken en verlof. Maar ik miste creativiteit en uitdaging tijdens dit werk. Daarom 
+ben ik begonnen aan de opleiding <strong>Communicatie en Multimedia Design</strong> aan de Hogeschool 
+Rotterdam. Dit om mezelf uit te dagen en bezig te zijn op creatief vlak. Ik heb geen achtergrond 
+in dit vakgebied. Wel heb ik ontdekt dat ik <strong>Visual design</strong> erg interessant vind, dit zou ik graag 
+willen combineren met <strong>productfotografie</strong>. Ik kan echt uren bezig zijn met het maken en 
+bekijken van foto's. En daarom wil ik mij verder ontwikkelen als Visual designer en 
+productfotograaf". 
+      </div>
+    </div>
+    <div class="col 12 col-md-6">
+      <div class="text mt-2">
+        <img src="<?= $site->url() ?>/media/pages/over-mij/1524864105-1587107495/00daphne.jpg" alt="About Me foto">
+      </div>
+    </div>
+  </div>
+</main>
+<?php snippet('footer') ?>
